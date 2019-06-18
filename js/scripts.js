589c3437e1be033e6a6279e999a992ce18c8f2f3
Devin Cooley <dcooley1350@gmail.com>
2019-06-18 13:01:44 -0700

    change repetitive code with arrays and loops.

--- js/scripts.js
+++ js/scripts.js
@@ -2,25 +2,13 @@ $(document).ready(function()  {
   $("#survey form").submit(function(event)  {
    
     event.preventDefault();
-    var firstName = $("input#FirstName").val();
-    var lastName = $("input#LastName").val();
-    var favAnimal = $("input#FavoriteAnimal").val();
-    var favFood = $("input#FavoriteFood").val();
-    var favPlace = $("input#FavoritePlace").val();
-    var favNumber = $("input#FavoriteNumber").val();
-    var favTeam = $("input#FavoriteTeam").val();
 
-    myArray = [firstName, lastName, favAnimal, favFood, favPlace, favTeam]
-
-    $(".FirstName").text(firstName);
-    $(".LastName").text(lastName);
-    $(".FavoriteAnimal").text(favAnimal);
-    $(".FavoriteFood").text(favFood);
-    $(".FavoritePlace").text(favPlace);
-    $(".FavoriteNumber").text(favNumber);
-    $(".FavoriteTeam").text(favTeam);
+    var datas = ["FirstName","LastName","FavoriteAnimal","FavoriteFood","FavoritePlace","FavoriteNumber","FavoriteTeam"];
+    datas.forEach(function(data) {
+      var userInput = $("input#" + data).val();
+        $("." + data).text(userInput)
+    })
 
     $(".summary").fadeIn();
-
   })
 })
